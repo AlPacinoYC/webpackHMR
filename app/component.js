339037092d9dae0e96c1e6918c3933cbb8f82ede
yuwanlin <1227620310@qq.com>
2017-03-17 22:04:57 +0800

    webpack热模块替换（HMR）

---
+++ app/component.js
@@ -0,0 +1,6 @@
+import {a} from './a';
+export default function () {
+  var element = document.createElement('h1');
+  element.innerHTML = a;
+  return element;
+}
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import component from './component';
+let demoComponent = component();
+document.body.appendChild(demoComponent);
+
+alert(1);
+//HMR 接口
+if(module.hot){
+    module.hot.accept('./component',()=>{
+        const nextComponent=component();
+        document.body.replaceChild(nextComponent,demoComponent);
+        demoComponent=nextComponent;
+    })
+}
